69676ef04a83b534f84b2c894ebf7c8046ebcaf5
jotas6 <j.oneto@pumas.ai>
2023-06-21 11:03:52 -0500

    Updated note in `create_chat`'s docstring

--- src/OpenAI.jl
+++ src/OpenAI.jl
@@ -236,7 +236,8 @@ Create chat
 - `temperature::Float64=1.0`: What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.
 - `top_p::Float64=1.0`: An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. We generally recommend altering this or temperature but not both.
 
-!!! note: do not use `stream=true` option here, instead use the `streamcallback` keyword argument (see the relevant section below).
+!!! note
+    Do not use `stream=true` option here, instead use the `streamcallback` keyword argument (see the relevant section below).
 
 For more details about the endpoint and additional arguments, visit https://platform.openai.com/docs/api-reference/chat
 
