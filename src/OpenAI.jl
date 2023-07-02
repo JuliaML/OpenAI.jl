a8c70e4486c8677c155dfc9279762519bd3d2dc9
jotas6 <j.oneto@pumas.ai>
2023-07-02 14:54:23 -0500

    Small fixes to `create_images` docstring

--- src/OpenAI.jl
+++ src/OpenAI.jl
@@ -344,18 +344,18 @@ Create images
 # Arguments: 
 - `api_key::String`: OpenAI API key
 - `prompt`: The input text to generate the image(s) for, as String or array of tokens.
-- `n`::Integer Optional. The number of images to generate. Must be between 1 and 10.
-- `size`::String. Optional. Defaults to 1024x1024. The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
+- `n::Integer`: Optional. The number of images to generate. Must be between 1 and 10.
+- `size::String`: Optional. Defaults to 1024x1024. The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
 
 # Keyword Arguments:
 - `http_kwargs::NamedTuple`: Optional. Keyword arguments to pass to HTTP.request.
-- `response_format`::String: Optional. Defaults to "url". The format of the response. Must be one of "url" or "b64_json".
+- `response_format::String`: Optional. Defaults to "url". The format of the response. Must be one of "url" or "b64_json".
 
 For additional details about the endpoint, visit <https://platform.openai.com/docs/api-reference/images/create>
 
 # once the request is made, 
 download like this: 
-download(r.response["data"][begin]["url"], "image.png")
+`download(r.response["data"][begin]["url"], "image.png")`
 """
 function create_images(api_key::String, prompt, n::Integer=1, size::String="256x256"; http_kwargs::NamedTuple=NamedTuple(), kwargs...)
     return openai_request("images/generations", api_key; method="POST", http_kwargs=http_kwargs, prompt, kwargs...)
