a0258b5244491499389ab83212c9bba4d252748c
Marius Fersigan <marius.fersigan@gmail.com>
2023-07-03 14:46:25 +0300

    tests bug fix + JET quality check

--- test/completion.jl
+++ test/completion.jl
@@ -2,57 +2,16 @@
   r = list_models(ENV["OPENAI_API_KEY"])
   if !=(r.status, 200)
     @test false
-  end
+  end  
   r = create_completion(
     ENV["OPENAI_API_KEY"],
-    r.response["data"][begin]["id"];
+    "text-ada-001";
     prompt="Say \"this is a test\""
   )
   println(r.response["choices"][begin]["text"])
   if !=(r.status, 200)
     @test false
-  end
-
-  r = create_chat(
-    ENV["OPENAI_API_KEY"],
-    "gpt-3.5-turbo",
-    [Dict("role" => "user", "content" => "What is the OpenAI mission?")]
-  )
-  println(r.response["choices"][begin]["message"]["content"])
-  if !=(r.status, 200)
-    @test false
-  end
-
-  # with http kwargs (with default values)
-  r = create_chat(
-    ENV["OPENAI_API_KEY"],
-    "gpt-3.5-turbo",
-    [Dict("role" => "user", "content" => "Summarize HTTP.jl package in one sentence.")],
-    http_kwargs=(connect_timeout=10, readtimeout=0)
-  )
-  println(r.response["choices"][begin]["message"]["content"])
-  if !=(r.status, 200)
-    @test false
-  end
-
-  # streaming chat
-  r = create_chat(
-    ENV["OPENAI_API_KEY"],
-    "gpt-3.5-turbo",
-    [Dict("role" => "user", "content" => "What continent is New York in? Two word answer.")],    
-    streamcallback=let
-      count = 0
-
-      function f(s::String)
-        count = count + 1
-        println("Chunk $count")
-      end
-    end)
-
-  println(map(r -> r["choices"][1]["delta"], r.response))
-  if !=(r.status, 200)
-    @test false
-  end
+  end  
 end
 
 @testset "create edit" begin
