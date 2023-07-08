c12ae6886e9f16198dc85a1d1e08145f61ae1f1e
Marius Fersigan <marius.fersigan@gmail.com>
2023-07-08 19:11:27 +0300

    tests bug fix + JET quality & type stability check (#40)
    * tests bug fix + JET quality check
    
    * prompt update
    
    * prompt change
    
    * deps update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,32 @@
 using OpenAI
+using JET
+using Pkg
 using Test
+
+function get_pkg_version(name::AbstractString)
+  for dep in values(Pkg.dependencies())
+      if dep.name == name
+          return dep.version
+      end
+  end
+  return error("Dependency not available")
+end
+
+@testset "Code quality (JET.jl)" begin
+  if VERSION >= v"1.9"
+      @assert get_pkg_version("JET") >= v"0.8.4"
+      JET.test_package(OpenAI; target_defined_modules=true)
+  end
+end
+
 
 @testset "OpenAI.jl" begin
   printstyled(color=:blue, "\n")
   @testset "models" begin
     include("models.jl")
+  end
+  @testset "chatcompletion" begin
+    include("chatcompletion.jl")
   end
   @testset "completion" begin
     include("completion.jl")
@@ -12,7 +34,7 @@ using Test
   @testset "embeddings" begin
     include("embeddings.jl")
   end
-  @testset "user usage" begin
+  @testset "usage" begin
     include("usage.jl")
-  end
+  end  
 end
