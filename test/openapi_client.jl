88718925c78661512f11a481809bf4c6ca5ec7a3
Rory Linehan <rorylinehan@protonmail.com>
2026-06-03 17:22:32 -0500

    add smoke test for generated client

--- test/openapi_client.jl
+++ test/openapi_client.jl
@@ -9,4 +9,11 @@
     models_api = OpenAIClient.ModelsApi(client)
     @test models_api isa OpenAIClient.ModelsApi
     @test OpenAIClient.basepath(OpenAIClient.ModelsApi) == "https://api.openai.com/v1"
+
+    live_client = openai_client(ENV["OPENAI_API_KEY"])
+    live_models_api = OpenAIClient.ModelsApi(live_client)
+    models, http_response = OpenAIClient.list_models(live_models_api)
+
+    @test http_response.status == 200
+    @test !isempty(models.data)
 end
